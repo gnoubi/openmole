17ba77756953641061890830cf3045a8e6e00ad2
Fabien De Vienne <fabien.devienne@gmail.com>
2013-06-14 14:04:01 +0200

    Correction of the SelectTask

---
+++ openmole/core/plugins/org.openmole.plugin.method.abc/src/main/scala/org/openmole/plugin/method/abc/Beaumont.scala
@@ -0,0 +1,12 @@
+package org.openmole.plugin.method.abc
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: backslash
+ * Date: 14/06/13
+ * Time: 13:43
+ * To change this template use File | Settings | File Templates.
+ */
+class Beaumont {
+
+}
